6dd008be6665f3b9c67d7ad9cf3c9be7bb222c74
Jay Thakkar <ce.jaythakkar@gmail.com>
2023-04-06 13:28:39 +0530

    updated next.js.config
    updated next.js.config

--- next.config.js
+++ next.config.js
@@ -47,7 +47,7 @@ module.exports = module.exports = (phase, { defaultConfig }) => {
   return {
     /* development only config options here */
     basePath: '/Travel',
-    assetPrefix: 'Travel',
+    assetPrefix: 'Travel/',
     ...nextConfig,
   };
 };
